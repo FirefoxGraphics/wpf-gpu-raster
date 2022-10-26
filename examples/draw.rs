dd66f841cfb0326ac7ce45aab570a09fd9f8be98
Jeff Muizelaar <jrmuizel@gmail.com>
2022-10-26 08:50:25 -0400

    Reimplement the triangle rasterizer.
    The new rasterizer uses fixed point and properly implements
    the fill rule so that pixels are not double painted.

--- examples/draw.rs
+++ examples/draw.rs
@@ -1,86 +1,46 @@
+/* The rasterization code here is based off of piglit/tests/general/triangle-rasterization.cpp:
+
+    /**************************************************************************
+     *
+     * Copyright 2012 VMware, Inc.
+     * All Rights Reserved.
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a
+     * copy of this software and associated documentation files (the
+     * "Software"), to deal in the Software without restriction, including
+     * without limitation the rights to use, copy, modify, merge, publish,
+     * distribute, sub license, and/or sell copies of the Software, and to
+     * permit persons to whom the Software is furnished to do so, subject to
+     * the following conditions:
+     *
+     * The above copyright notice and this permission notice (including the
+     * next paragraph) shall be included in all copies or substantial portions
+     * of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+     * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+     * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+     * IN NO EVENT SHALL VMWARE AND/OR ITS SUPPLIERS BE LIABLE FOR
+     * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+     * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+     * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+     *
+     **************************************************************************/
+
+*/
+
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 use euclid::{default::Transform2D, point2};
 use wpf_gpu_raster::{PathBuilder};
 
 
-use std::{convert::TryInto, ops::{Index, Mul, Sub, IndexMut, AddAssign}};
-#[derive(Clone, Copy, Debug)]
-struct vec<T, const M: usize> {
-    data_: [T; M]
-}
+use std::ops::Index;
 
-impl<T: Default, const N: usize> Default for vec<T, N> where [T; N]: Default {
-    fn default() -> Self {
-        Self { data_: Default::default() }
-    }
-}
-impl<T: Copy, const N: usize> vec<T, N> {
-    fn new(data: &[T]) -> Self {
-        vec { data_: data.try_into().unwrap() }
-    }
-}
-
-impl<T, const N: usize> Index<usize> for vec<T, N>  {
-    type Output = T;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.data_[index]
-    }
-}
-
-impl<T, const N: usize> IndexMut<usize> for vec<T, N>  {
-
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.data_[index]
-    }
-}
-
-impl<T, const N: usize> Mul for vec<T, N> where T: Copy + Default + Mul + AddAssign<<T as Mul>::Output>  {
-    type Output = T;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        let mut result = Default::default();
-        for i in 0..N {
-            result += self[i] * rhs[i]
-        }
-        result
-    }
-}
-
-
-type Vec2f = vec<f32, 2>;
-type Vec2i = vec<i32, 2>;
-type Vec3f = vec<f32, 3>;
-
-impl From<Vec2i> for Vec2f {
-    fn from(v: Vec2i) -> Self {
-        Vec2f::new(&[v[0] as f32, v[1] as f32])
-    }
-}
-
-fn cross<T>(v1: vec<T, 3>, v2: vec<T, 3>) -> vec<T, 3> where T: Copy + Mul<Output = T> + Sub<Output = T> {
-    vec::<T, 3>::new(&[v1[1]*v2[2] - v1[2]*v2[1], v1[2]*v2[0] - v1[0]*v2[2], v1[0]*v2[1] - v1[1]*v2[0]])
-}
 
 const  WIDTH: u32  = 800;
 const HEIGHT: u32  = 800;
 
-#[derive(Clone, Copy)]
-struct TGAColor(u8, u8, u8, u8);
-
-
-impl TGAColor {
-    fn new(a: u8, r: u8, g: u8, b: u8) -> Self { TGAColor(a, r, g, b) }
-}
-
-impl Mul<f32> for TGAColor {
-    type Output = TGAColor;
-
-    fn mul(self, rhs: f32) -> Self::Output {
-        TGAColor((self.0 as f32 * rhs) as u8, (self.1 as f32 * rhs) as u8, (self.2 as f32 * rhs) as u8, (self.3 as f32 * rhs) as u8)
-    }
-}
 
 fn over(src: u32, dst: u32) -> u32 {
     let a = src >> 24;
@@ -108,100 +68,191 @@ fn over(src: u32, dst: u32) -> u32 {
     (ag << 8) + rb
 }
 
-struct TGAImage {
-    buf: Vec<u8>,
-    width: u32,
-    height: u32
+pub fn alpha_mul(x: u32, a: u32) -> u32 {
+    let mask = 0xFF00FF;
+
+    let src_rb = ((x & mask) * a) >> 8;
+    let src_ag = ((x >> 8) & mask) * a;
+
+    (src_rb & mask) | (src_ag & !mask)
 }
 
-impl TGAImage {
-    fn new(width: u32, height: u32) -> Self { Self { width, height, buf: vec![0; (width * height * 3) as usize] } }
-    /*fn set(&mut self, x: i32, y: i32, color: TGAColor) {
-        if x >= self.width as i32 || y >= self.height as i32 {
-            return;
-        }
-        self.buf[((x + y * self.width as i32) * 3) as usize] = color.0;
-        self.buf[((x + y * self.width as i32) * 3 + 1) as usize] = color.1;
-        self.buf[((x + y * self.width as i32) * 3 + 2) as usize] = color.2;
-    }*/
-    fn blend(&mut self, x: i32, y: i32, color: TGAColor) {
-        if x >= self.width as i32 || y >= self.height as i32 {
-            return;
-        }
-        let index = ((x + y * self.width as i32) * 3) as usize;
-        let dst = 0xff << 24 |
-                     (self.buf[index] as u32) << 16 |
-                     (self.buf[index + 1] as u32) << 8 |
-                     (self.buf[index + 2] as u32);
-        let src = (color.0 as u32) << 24 |
-                     (color.1 as u32) << 16 |
-                     (color.2  as u32) << 8 |
-                     color.3 as u32;
-        let dst = over(src, dst);
-        self.buf[index] = ((dst >> 16) & 0xff) as u8;
-        self.buf[index + 1] = ((dst >> 8) & 0xff) as u8;
-        self.buf[index + 2] = ((dst >> 0) & 0xff) as u8;
+fn write_image(data: &[u32], path: &str) {
+    use std::path::Path;
+    use std::fs::File;
+    use std::io::BufWriter;
+
+    let mut png_data: Vec<u8> = vec![0; (WIDTH * HEIGHT * 3) as usize];
+    let mut i = 0;
+    for pixel in data {
+        png_data[i] = ((pixel >> 16) & 0xff) as u8;
+        png_data[i + 1] = ((pixel >> 8) & 0xff) as u8;
+        png_data[i + 2] = ((pixel >> 0) & 0xff) as u8;
+        i += 3;
     }
-    fn write(&self, path: &str) {
-        use std::path::Path;
-        use std::fs::File;
-        use std::io::BufWriter;
 
-        let path = Path::new(path);
-        let file = File::create(path).unwrap();
-        let w = &mut BufWriter::new(file);
 
-        let mut encoder = png::Encoder::new(w, self.width, self.height); // Width is 2 pixels and height is 1.
-        encoder.set_color(png::ColorType::Rgb);
-        encoder.set_depth(png::BitDepth::Eight);
-        let mut writer = encoder.write_header().unwrap();
+    let path = Path::new(path);
+    let file = File::create(path).unwrap();
+    let w = &mut BufWriter::new(file);
 
-        writer.write_image_data(&self.buf).unwrap(); // Save
+    let mut encoder = png::Encoder::new(w, WIDTH, HEIGHT); // Width is 2 pixels and height is 1.
+    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+
+    writer.write_image_data(&png_data).unwrap(); // Save
+}
+
+#[derive(Debug)]
+struct Vertex {
+    x: f32,
+    y: f32,
+    coverage: f32
+}
+#[derive(Debug)]
+struct Triangle {
+    v: [Vertex; 3],
+}
+
+impl Index<usize> for Triangle  {
+    type Output = Vertex;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.v[index]
     }
 }
 
+// D3D11 mandates 8 bit subpixel precision:
+// https://microsoft.github.io/DirectX-Specs/d3d/archive/D3D11_3_FunctionalSpec.htm#CoordinateSnapping
+const FIXED_SHIFT: i32 = 8;
+const FIXED_ONE: f32 = (1 << FIXED_SHIFT) as f32;
 
-fn barycentric(A: Vec2f, B: Vec2f, C: Vec2f, P: Vec2f) -> Vec3f {
-    let mut s: [Vec3f; 2] = Default::default();
-    for i in (0..2).rev() {
-        s[i][0] = C[i]-A[i];
-        s[i][1] = B[i]-A[i];
-        s[i][2] = A[i]-P[i];
+/* Proper rounding of float to integer */
+fn iround(mut v: f32) -> i64 {
+    if v > 0.0 {
+        v += 0.5;
     }
-    let u = cross(s[0], s[1]);
-    if u[2].abs()>1e-2 {// dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
-        return Vec3f::new(&[1.-(u[0]+u[1])/u[2], u[1]/u[2], u[0]/u[2]]);
+    if v < 0.0 {
+        v -= 0.5;
     }
-    Vec3f::new(&[-1.,1.,1.]) // in this case generate negative coordinates, it will be thrown away by the rasterizator
+    return v as i64
 }
 
-fn triangle(pts: &[Vec2f], coverage: Vec3f, image: &mut TGAImage, color: TGAColor) {
-    let mut bboxmin = Vec2f::new( &[f32::MAX,  f32::MAX]);
-    let mut bboxmax = Vec2f::new(&[-f32::MAX, -f32::MAX]);
-    for i in 0..3 {
-        for j in 0..2 {
-            bboxmin[j] = bboxmin[j].min(pts[i][j]);
-            bboxmax[j] = bboxmax[j].max(pts[i][j]);
-        }
+/* Based on http://devmaster.net/forums/topic/1145-advanced-rasterization */
+fn rast_triangle(buffer: &mut [u32], stride: usize, tri: &Triangle, color: u32) {
+    let center_offset = -0.5;
+
+    let mut coverage1 = tri[0].coverage;
+    let mut coverage2 = tri[1].coverage;
+    let mut coverage3 = tri[2].coverage;
+
+    /* fixed point coordinates */
+    let mut x1 = iround(FIXED_ONE * (tri[0].x + center_offset));
+    let     x2 = iround(FIXED_ONE * (tri[1].x + center_offset));
+    let mut x3 = iround(FIXED_ONE * (tri[2].x + center_offset));
+
+    let mut y1 = iround(FIXED_ONE * (tri[0].y + center_offset));
+    let     y2 = iround(FIXED_ONE * (tri[1].y + center_offset));
+    let mut y3 = iround(FIXED_ONE * (tri[2].y + center_offset));
+
+
+    /* Force correct vertex order */
+    let cross = (x2 - x1) * (y3 - y2) - (y2 - y1) * (x3 - x2);
+    if cross > 0 {
+        std::mem::swap(&mut x1, &mut x3);
+        std::mem::swap(&mut y1, &mut y3);
+        // I don't understand why coverage 2 and 3 are swapped instead of 1 and 3
+        std::mem::swap(&mut coverage2, &mut coverage3);
+    } else {
+        std::mem::swap(&mut coverage1, &mut coverage3);
     }
-    for x in (bboxmin[0] as i32)..=(bboxmax[0] as i32) {
-        for y in (bboxmin[1] as i32)..=(bboxmax[1] as i32) {
-            let P = Vec2i::new(&[x, y]);
-            let c = barycentric(pts[0], pts[1], pts[2], P.into());
-            if c[0]<0. || c[1]<0. || c[2]<0. { continue };
-            let coverage = coverage * c;
-            let color = color * coverage;
-            image.blend(P[0], P[1], color);
+    
+    /* Deltas */
+    let dx12 = x1 - x2;
+    let dx23 = x2 - x3;
+    let dx31 = x3 - x1;
+
+    let dy12 = y1 - y2;
+    let dy23 = y2 - y3;
+    let dy31 = y3 - y1;
+
+    /* Fixed-point deltas */
+    let fdx12 = dx12 << FIXED_SHIFT;
+    let fdx23 = dx23 << FIXED_SHIFT;
+    let fdx31 = dx31 << FIXED_SHIFT;
+
+    let fdy12 = dy12 << FIXED_SHIFT;
+    let fdy23 = dy23 << FIXED_SHIFT;
+    let fdy31 = dy31 << FIXED_SHIFT;
+
+    /* Bounding rectangle */
+    let mut minx = x1.min(x2).min(x3) >> FIXED_SHIFT;
+    let mut maxx = x1.max(x2).max(x3) >> FIXED_SHIFT;
+
+    let mut miny = y1.min(y2).min(y3) >> FIXED_SHIFT;
+    let mut maxy = y1.max(y2).max(y3) >> FIXED_SHIFT;
+
+    minx = minx.max(0);
+    maxx = maxx.min(WIDTH as i64 - 1);
+
+    miny = miny.max(0);
+    maxy = maxy.min(HEIGHT as i64 - 1);
+
+    /* Half-edge constants */
+    let mut c1 = dy12 * x1 - dx12 * y1;
+    let mut c2 = dy23 * x2 - dx23 * y2;
+    let mut c3 = dy31 * x3 - dx31 * y3;
+
+    /* Correct for top-left filling convention */
+    if dy12 < 0 || (dy12 == 0 && dx12 < 0) { c1 += 1 }
+    if dy23 < 0 || (dy23 == 0 && dx23 < 0) { c2 += 1 }
+    if dy31 < 0 || (dy31 == 0 && dx31 < 0) { c3 += 1 }
+
+    let mut cy1 = c1 + dx12 * (miny << FIXED_SHIFT) - dy12 * (minx << FIXED_SHIFT);
+    let mut cy2 = c2 + dx23 * (miny << FIXED_SHIFT) - dy23 * (minx << FIXED_SHIFT);
+    let mut cy3 = c3 + dx31 * (miny << FIXED_SHIFT) - dy31 * (minx << FIXED_SHIFT);
+
+    /* Perform rasterization */
+    let mut buffer = &mut buffer[miny as usize * stride..];
+    for _y in miny..=maxy {
+        let mut cx1 = cy1;
+        let mut cx2 = cy2;
+        let mut cx3 = cy3;
+
+        for x in minx..=maxx {
+            if cx1 > 0 && cx2 > 0 && cx3 > 0 {
+                // cross is equal to 2*area of the triangle.
+                // we can normalize cx by 2*area to get barycentric coords.
+                let area = cross.abs() as f32;
+                let bary = (cx1 as f32 / area, cx2 as f32/ area, cx3 as f32 / area);
+
+                let coverages = coverage1 * bary.0 + coverage2 * bary.1 + coverage3 * bary.2;
+
+                let color = alpha_mul(color, (coverages * 256. + 0.5) as u32);
+                buffer[x as usize] = over(color, buffer[x as usize]);
+            }
+
+            cx1 -= fdy12;
+            cx2 -= fdy23;
+            cx3 -= fdy31;
         }
+
+        cy1 += fdx12;
+        cy2 += fdx23;
+        cy3 += fdx31;
+
+        buffer = &mut buffer[stride..];
     }
 }
+
 
 fn main() {
     let opt = usvg::Options::default();
 
     let rtree = usvg::Tree::from_file("tiger.svg", &opt).unwrap();
 
-    let mut image = TGAImage::new(WIDTH, HEIGHT);
+    let mut image = vec![0; (WIDTH * HEIGHT) as usize];
     for _ in 0..1 {
     let mut total_vertex_count = 0;
     let mut total_time = std::time::Duration::default();
@@ -220,8 +271,8 @@ fn main() {
             let color = match usvg_path.fill {
                 Some(ref fill) => {
                     match fill.paint {
-                        usvg::Paint::Color(c) => TGAColor::new(255, c.red, c.green, c.blue),
-                        _ => TGAColor::new(255, 0, 255, 0),
+                        usvg::Paint::Color(c) => 0xff000000 | (c.red as u32) << 16 | (c.green as u32) << 8 | c.blue as u32,
+                        _ => 0xff00ff00,
                     }
                 }
                 None => {
@@ -255,7 +306,7 @@ fn main() {
                     }
                 }
             }
-            let start = std::time::Instant::now();
+            let start = std::time::Instant::now(); 
             let result = builder.rasterize_to_tri_strip(0, 0, WIDTH as i32, HEIGHT as i32);
             let end = std::time::Instant::now();
             total_time += end - start;
@@ -273,14 +324,14 @@ fn main() {
                     [&result[n+1], &result[n], &result[n+2]]
                 };
 
-                let mut screen_coords: [Vec2f; 3] = Default::default();
-                let mut coverage: Vec3f = Default::default();
-                for j in 0..3 {
-                    let vertex = vertices[j];
-                    screen_coords[j] = Vec2f::new(&[vertex.x - 0.5, vertex.y - 0.5]);
-                    coverage[j] = vertex.coverage;                 
-                }
-                triangle(&screen_coords, coverage, &mut image, color);
+                let src = color;
+                let tri = Triangle { v: [
+                    Vertex { x: vertices[0].x, y: vertices[0].y, coverage: vertices[0].coverage},
+                    Vertex { x: vertices[1].x, y: vertices[1].y, coverage: vertices[1].coverage},
+                    Vertex { x: vertices[2].x, y: vertices[2].y, coverage: vertices[2].coverage}
+                    ]
+                };
+                rast_triangle(&mut image, WIDTH as usize, &tri, src);
             }
         }
     }
@@ -289,7 +340,7 @@ fn main() {
     }
 
 
-    image.write("out.png");
+    write_image(&image, "out.png");
     use std::{hash::{Hash, Hasher}, collections::hash_map::DefaultHasher};
     use crate::*;
     fn calculate_hash<T: Hash>(t: &T) -> u64 {
@@ -298,8 +349,8 @@ fn main() {
         s.finish()
     }
 
-    assert_eq!(calculate_hash(&image.buf),
-        if cfg!(debug_assertions) { 0xb03d06a10a8353a1 } else { 0x4351654ca83459c0});
+    assert_eq!(calculate_hash(&image),
+        if cfg!(debug_assertions) { 0x5973c52a1c0232f3 } else { 0xf15821a5bebc5ecf});
 
 
 }
