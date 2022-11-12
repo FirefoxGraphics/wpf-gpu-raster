2cc28265cd32b11324c664ddfe9a174b0a150c21
Lee Salzman <lsalzman@gmail.com>
2022-11-12 01:54:26 -0500

    use x86 instead of x86_64 arch

--- src/real.rs
+++ src/real.rs
@@ -9,7 +9,7 @@ pub mod CFloatFPU {
         //XXX: the SSE2 version is probably slower than a naive SSE4 implementation that can use roundss
         #[cfg(target_feature = "sse2")]
         unsafe {
-            use std::arch::x86_64::{__m128, _mm_set_ss, _mm_cvtss_si32, _mm_cvtsi32_ss, _mm_sub_ss, _mm_cmple_ss, _mm_store_ss, _mm_setzero_ps};
+            use std::arch::x86::{__m128, _mm_set_ss, _mm_cvtss_si32, _mm_cvtsi32_ss, _mm_sub_ss, _mm_cmple_ss, _mm_store_ss, _mm_setzero_ps};
 
             let given: __m128 = _mm_set_ss(x);                       // load given value
             let result = _mm_cvtss_si32(given);
